8da9d26fcef7f65c7be13235ee15edcdba5de2a7
Ode42 <ode.progr@outlook.com>
2022-06-06 21:15:55 +0300

    Pelilauta

---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Pelilauta from "./Pelilauta";
+import "./../styles/Peli.css";
+
+export default function Wrapper() {
+  return (
+    <div>
+      <Pelilauta />
+    </div>
+  );
+}
